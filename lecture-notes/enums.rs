318fe952bed00b14b99291490e8b3bdd812c5a57
Stanley Huang <stanleyh201k@gmail.com>
2026-02-27 14:09:26 -0500

    fix code

--- lecture-notes/enums.rs
+++ lecture-notes/enums.rs
@@ -11,14 +11,13 @@ enum TrafficLight {
 // Takes up as much memory as 3 u32 
 // Smallest amount is 1 byte 
 // Your code here
-use TrafficLight::*;
 
 fn next_light(light: TrafficLight) -> TrafficLight {
     match light {
         TrafficLight::Red(_) => TrafficLight::Green(30),
-        TrafficLight::Green(30) => TrafficLight::Yellow(5),
-        TrafficLight::Yellow(5) => TrafficLight::Red(45),
-    };
+        TrafficLight::Green(_) => TrafficLight::Yellow(5),
+        TrafficLight::Yellow(_) => TrafficLight::Red(45),
+    }
 }
 
 fn get_light_color(light: &TrafficLight) -> &str {
@@ -26,29 +25,30 @@ fn get_light_color(light: &TrafficLight) -> &str {
         TrafficLight::Red(num) => "Red",
         TrafficLight::Green(num) => "Green",
         TrafficLight::Yellow(num) => "Yellow",
-    };
+    }
 }
 
 fn get_time_remaining(light: &TrafficLight) -> u32 {
    match light {
-       TrafficLight::Red(time) => time,
-       TrafficLight::Green(time) => time,
-       TrafficLight::Yellow(time) => time,
-   };
+       TrafficLight::Red(time) => *time,
+       TrafficLight::Green(time) => *time,
+       TrafficLight::Yellow(time) => *time,
+   }
 }
 
 
 fn main() {
-    let light: TrafficLight = TrafficLight::Red(45); 
+    let mut light: TrafficLight = TrafficLight::Red(45); 
     
     for _ in 0..3 {
-        next_light(light); 
+        light = next_light(light); 
+        println!(
+            "Light: {}, Time remaining: {}",
+            get_light_color(&light),
+            get_time_remaining(&light)
+    );
     };
     
-    println!(
-        "Light: {}, Time remaining: {}",
-        get_light_color(&light),
-        get_time_remaining(&light)
-    );
+    
 }
 
