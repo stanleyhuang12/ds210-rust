43c16d0bc950ffdb92a3bd7d5df56f9acd481094
Stanley Huang <stanleyh201k@gmail.com>
2026-02-27 12:51:21 -0500

    enums try

---
+++ lecture-notes/enums.rs
@@ -0,0 +1,54 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum TrafficLight {
+    Red(u32),    // seconds remaining
+    Yellow(u32), // seconds remaining  
+    Green(u32),  // seconds remaining
+}
+
+// Takes up as much memory as 3 u32 
+// Smallest amount is 1 byte 
+// Your code here
+use TrafficLight::*;
+
+fn next_light(light: TrafficLight) -> TrafficLight {
+    match light {
+        TrafficLight::Red(_) => TrafficLight::Green(30),
+        TrafficLight::Green(30) => TrafficLight::Yellow(5),
+        TrafficLight::Yellow(5) => TrafficLight::Red(45),
+    };
+}
+
+fn get_light_color(light: &TrafficLight) -> &str {
+    match light {
+        TrafficLight::Red(num) => "Red",
+        TrafficLight::Green(num) => "Green",
+        TrafficLight::Yellow(num) => "Yellow",
+    };
+}
+
+fn get_time_remaining(light: &TrafficLight) -> u32 {
+   match light {
+       TrafficLight::Red(time) => time,
+       TrafficLight::Green(time) => time,
+       TrafficLight::Yellow(time) => time,
+   };
+}
+
+
+fn main() {
+    let light: TrafficLight = TrafficLight::Red(45); 
+    
+    for _ in 0..3 {
+        next_light(light); 
+    };
+    
+    println!(
+        "Light: {}, Time remaining: {}",
+        get_light_color(&light),
+        get_time_remaining(&light)
+    );
+}
+
