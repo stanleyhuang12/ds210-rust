da5b9fc5ae55fe44a0dff24d8b704a07d3220cfa
Stanley Huang <stanleyh201k@gmail.com>
2026-03-02 08:50:56 -0500

    more updates

--- exam-prep/mini-calculator/src/main.rs
+++ exam-prep/mini-calculator/src/main.rs
@@ -1,16 +1,22 @@
-use::std::io; 
-
+use std::io; 
+use rand::Rng; 
+use std::cmp::Ordering;
 fn main() {
-    let secret_number: u8 = 200; 
-    let mut guess = String::new(); 
-    println!("This is a secret guessing game. Values are between 0 and 255. ");
-
-    io::stdin().read_line(&mut guess).expect("Error reading lines.");
-    let guess_number: u8 = guess.trim().parse().expect("Error parsing number.");
-
-    if guess_number == secret_number {
-        println!("You have correctly guessed that the secret number is {}", guess_number);
-    } else {
-        println!("You have incorrectly guessed the secret number.");
+    /*** Dynamic guessing game  */
+    let secret_number =  rand::rng().random_range(1..=100); //Values can take on between 0 and 100 inclusive 
+    println!("Secret number {}", secret_number);
+    println!("This is a secret guessing game. Values are between 1 and 100. ");
+    loop {
+        let mut guess = String::new(); 
+        io::stdin().read_line(&mut guess).expect("Error reading lines.");
+        let num_guess: u8 = guess.trim().parse().expect("Error parsing number.");
+        match num_guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess is smaller."),
+            Ordering::Greater => println!("Your guess is greater."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
