cad8cf1148fab138bf9a64f450b81cff3f29cf11
Stanley Huang <stanleyh201k@gmail.com>
2026-03-17 12:55:04 -0400

    ownership notes and explanations

--- lecture-notes/ownership.rs
+++ lecture-notes/ownership.rs
@@ -35,3 +35,12 @@ fn add_numbers(v: &mut Vec<i32>, new_nums: Vec<i32>) {
         v.push(num);
     }
 }
+
+/*
+For the first operation `calculate_sum`, we only needed an immutable reference because we aren't trying to make underlying modifications of the data. We just need to read out the values. So I added a & before the variable and changed the typing to reflect it. 
+
+The second operation `double_values` required a modification of the data. Thus, I use &mut so that the numbers ownership does not get passed into the function which could lead to an out of scope error later when the function exits without returning the modification of the data. &mut enables "writing" or modifying the data (on the heap, in this case since it is a Vec<T>) . I also used *n to "dereference" and get the actual values of the vector instead of the pointer value. 
+
+Finally, similar logic applies for the last operation but does not require dereferencing because it is not referencing to an individual elements within a mutable reference. 
+
+*/
